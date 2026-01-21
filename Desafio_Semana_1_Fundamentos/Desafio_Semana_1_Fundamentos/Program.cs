33da07b76f1b602049ac1445cb09c95e7d0bf4c5
Carolinee <freitascaroline.92@gmail.com>
2026-01-21 07:30:05 -0400

    semana 1 commit 2

---
+++ Desafio_Semana_1_Fundamentos/Desafio_Semana_1_Fundamentos/Program.cs
@@ -0,0 +1,43 @@
+﻿string resposta;
+List<int> numeros = new List<int>();
+int num = 0;
+
+do
+{
+    Console.WriteLine("Deseja adicionar um número? S/N");
+    resposta = Console.ReadLine().ToUpper();
+    if (resposta == "S" || resposta == "N")
+    {
+        if (resposta == "S")
+        {
+            Console.WriteLine("Número: ");
+            num = int.Parse(Console.ReadLine());
+            numeros.Add(num);
+        }
+        else
+        {
+        
+            Console.WriteLine($"Sua lista de númeors é: ");
+            foreach (var n in numeros)
+            {
+                Console.WriteLine(n);
+    
+            }
+
+            Console.WriteLine($"A soma deles é: {numeros.Sum()}");
+            Console.WriteLine($"A média deles é: {numeros.Average()}");
+            Console.WriteLine($"O maior número é: {numeros.Max()}");
+            Console.WriteLine($"O menor número é: {numeros.Min()}");
+
+        }
+    }
+    else
+    {
+        Console.WriteLine("Valor inválido!!");
+        Console.WriteLine("Digite uma resposta válida...");
+        Console.WriteLine();
+        Console.WriteLine("Deseja adicionar um número? S/N");
+        resposta = Console.ReadLine().ToUpper();
+    }
+        
+} while (resposta.ToUpper() == "S");
